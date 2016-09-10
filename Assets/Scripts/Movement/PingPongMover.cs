0160ff09afe147a91995826550da25f3fc7b829b
Gaming <fearoo90@gmail.com>
2016-09-11 01:53:02 +0300

    PingPongMover: Initialized the class and changed Mover & StraitghtMover.

--- Assets/Scripts/Movement/PingPongMover.cs
+++ Assets/Scripts/Movement/PingPongMover.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PingPongMover : MonoBehaviour {
+public class PingPongMover : Mover{
 
+	[Range(-20, 20)]
+	public float x, y, z;
+	public float speed;
+
+	private Vector3 initialPosition;
+	
 	// Use this for initialization
 	void Start () {
-	
+		initialPosition = transform.position;
+		rigid.velocity += 
 	}
 	
 	// Update is called once per frame
