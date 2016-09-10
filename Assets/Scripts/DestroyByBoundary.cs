74f14bd12450b878c2999a3cad5958b872d3e750
Gaming <fearoo90@gmail.com>
2016-09-10 03:59:45 +0300

    Added LockPosition. Still with bugs.

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -3,12 +3,14 @@ using System.Collections;
 
 public class DestroyByBoundary : MonoBehaviour {
 
-	void OnTriggerExit(Collider other)
-    {
-		Destructible destructible = other.GetComponent<Destructible>();
-    	if (destructible != null) {
-       		other.GetComponent<Destructible>().Destruct();
-       	}
+	void OnTriggerExit (Collider other)
+	{
+		Destructible destructible = other.GetComponent<Destructible> ();
+		if (destructible != null) {
+			other.GetComponent<Destructible> ().Destruct ();
+		} else {
+			Destroy(other);
+		}
 //        Debug.Log (other);
     }
 }
---
+++ Assets/Scripts/Helpers/LockVelocity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockVelocity : MonoBehaviour {
+
+	public bool x, y, z;
+
+	private Vector3  lockedVelocity;
+	private Rigidbody rigid;
+
+	// Use this for initialization
+	void Start () {
+		rigid = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		lockedVelocity = rigid.velocity;
+		if (x) {
+			lockedVelocity.x = 0;
+		}
+		if (y) {
+			lockedVelocity.y = 0;
+		}
+		if (z) {
+			lockedVelocity.z = 0;
+		}
+
+		float multiplier = rigid.velocity.magnitude / lockedVelocity.magnitude;
+		Vector3 vect = lockedVelocity * multiplier;
+		rigid.velocity = lockedVelocity * multiplier;
+	}
+}
