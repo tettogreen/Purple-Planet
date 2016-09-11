9ee6d2b58bce8b039e99622d67741019eddd2630
Gaming <fearoo90@gmail.com>
2016-09-11 03:30:54 +0300

    Finished and tested PingPongMover. Currently only along X Axis.

--- Assets/Scripts/AI/SimpleWeaponAI.cs
+++ Assets/Scripts/AI/SimpleWeaponAI.cs
@@ -12,9 +12,12 @@ public class SimpleWeaponAI : MonoBehaviour {
 		//Check if target is in front of the object (shootable)
 //		var direction = transform.TransformPoint(transform.up);
 //		bool targertLocked = Physics.Raycast (transform.position, direction, viewDistance);
-		if ( (other.tag == "Player" || other.tag == "Neutral")) {
-			weapon.StartFire();
-		}
+		if ((other.tag == "Player" || other.tag == "Neutral")) {
+			weapon.StartFire ();
+		} 
+//		else if (other.tag == "Enemy") {
+//			Physics.Raycast(
+//		}
 	}
 
 	void OnTriggerExit (Collider other)
