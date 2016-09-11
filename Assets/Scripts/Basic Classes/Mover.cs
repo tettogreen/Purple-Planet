f7056d766443a73ccc80362c8d938e7494aa1632
Gaming <fearoo90@gmail.com>
2016-09-11 04:16:50 +0300

    Mover: Added lastVelocityModification.

--- Assets/Scripts/Basic Classes/Mover.cs
+++ Assets/Scripts/Basic Classes/Mover.cs
@@ -6,6 +6,10 @@ using System.Collections;
 public class Mover : MonoBehaviour {
 	protected Rigidbody rigid;
 
+
+	//TODO Create a variable which will contain the last mover's velocity modification to revert it.
+	protected Vector3 lastVelocityModification;
+
 	// Use this for initialization
 	void Awake () {
 		rigid = GetComponent<Rigidbody>();	
