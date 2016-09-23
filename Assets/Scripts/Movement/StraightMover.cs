201c9e79b9b7978eb66ec972711f0f3ea71d94ff
Gaming <fearoo90@gmail.com>
2016-09-24 01:54:43 +0300

    Straight Mover: Changed velocity to force moving.

--- Assets/Scripts/Movement/StraightMover.cs
+++ Assets/Scripts/Movement/StraightMover.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class StraightMover : Mover {
 
-	public float speed;
+	//public float speed;
+	public float acceleration;
+	public float accelerationTime = 1.2f;
 
 	//Time to allign course if it was changed
 	public float turnSpeed;
@@ -18,17 +20,33 @@ public class StraightMover : Mover {
 
 	[Range(-1, 1)]
 	public int direcionZ;
-	public bool useGlobalSpace;
-	
 
-	private Vector3 velocity;
+	//public int fleetModifier;				//Acceleration modifier value during fleet (F = m * a * accelerationModifier)
+	public bool useGlobalSpace;
+
+//	private Vector3 velocity;
+	private float maxVelocity;
+	//private int accelerationModifier;				//Current state of acceleration modifier (F = m * a * accelerationModifier)
+	private Vector3 airResistance;
+	private float mass;
+	private Vector3 thrust;
+	private Vector3 resistanceForce;
+	private Vector3 accelerationVector;
+
+
 	// Use this for initialization
 	void Start ()
 	{
 		if (useGlobalSpace) {
-			SetVelocity (Vector3.right, Vector3.up, Vector3.forward);
+			SetAccelerationDirection (Vector3.right, Vector3.up, Vector3.forward);
 		} else {
-			SetVelocity (transform.right, transform.up, transform.forward);
+			SetAccelerationDirection (transform.right, transform.up, transform.forward);
+		}
+
+		maxVelocity = acceleration * accelerationTime;
+		//Rigid initialization...
+		if (rigid) {
+			mass = rigid.mass;
 		}
 	}
 
@@ -42,42 +60,44 @@ public class StraightMover : Mover {
 	{
 		//Moving object if it has no Rigidbody..
 		if (!rigid) {
-			transform.position += velocity * Time.deltaTime;
-		}
-		//..Otherwise check if object should allign it's course
-		else if (alignVelocity) {
-
-			if (rigid.velocity != velocity) {
-				StartCoroutine (AlignVelocity ());
-			} else {
-				StopAllCoroutines ();
-			}
-
-		}
-		Vector3 acceleration;
-		Vector3 force = rigid.mass * acceleration;
-		if (rigid.velocity >= maxVelocity) {
-			resistanceForce = -force;
+				transform.position += accelerationVector.normalized * maxVelocity * Time.deltaTime;
 		} else {
-			resistanceForce = -rigid.mass * acceleration * Mathf.SmoothDamp (0f, resistanceForce,);
+			//Calculation of thrust force
+			thrust = mass * accelerationVector;
 
+			if (rigid.velocity.magnitude >= maxVelocity) {
+				resistanceForce = -thrust;
+			} else {
+				float modifier = rigid.velocity.magnitude / maxVelocity;
+				resistanceForce = -new Vector3 (Mathf.Pow (thrust.x, modifier), Mathf.Pow (thrust.y, modifier), Mathf.Pow (thrust.z, modifier));
+			}
+			rigid.AddForce(thrust + resistanceForce);
 		}
-		rigid.AddForce(force + resistanceForce);
+//		//..Otherwise check if object should allign it's course
+//		else if (alignVelocity) {
+//
+//			if (rigid.velocity != velocity) {
+//				StartCoroutine (AlignVelocity ());
+//			} else {
+//				StopAllCoroutines ();
+//			}
+//
+//		}
 	}
-
-	IEnumerator AlignVelocity ()
-	{
-		
-		yield return new WaitForSeconds(timeBeforeAlign);
-		rigid.velocity = Vector3.Lerp(rigid.velocity, velocity, speed * turnSpeed );
-	}
+//
+//	IEnumerator AlignVelocity ()
+//	{
+//		
+//		yield return new WaitForSeconds(timeBeforeAlign);
+//		rigid.velocity = Vector3.Lerp(rigid.velocity, velocity, speed * turnSpeed );
+//	}
 
 	// Sets velocity according to input axis
-	void SetVelocity (Vector3 xAxis, Vector3 yAxis, Vector3 zAxis)
+	void SetAccelerationDirection (Vector3 xAxis, Vector3 yAxis, Vector3 zAxis)
 	{
 		//Calculate velocity depending on the directions
-		velocity = xAxis * direcionX + yAxis * direcionY + zAxis * direcionZ;
-		velocity *= speed;
+		Vector3 accelerationDirection = xAxis.normalized * direcionX + yAxis.normalized * direcionY + zAxis.normalized * direcionZ;
+		accelerationVector = acceleration * accelerationDirection.normalized;
 
 		//Add relative objects velocity if it exists
 //		if (transform.parent) {
@@ -85,10 +105,11 @@ public class StraightMover : Mover {
 //		}
 
 		//Set velocity
-		if (rigid) {
-			velocity += rigid.velocity;
-			rigid.velocity = velocity;
-		}
+
+//		if (rigid) {
+//			velocity += rigid.velocity;
+//			rigid.velocity = velocity;
+//		}
 	}
 }
 	
